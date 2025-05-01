a836fe0f358a1a3e27cd4e494d3d414eace83ad6
StevenSetiawan <stevensetiawan1802@gmail.com>
2025-05-02 00:20:37 +0700

    [REFACTOR] Add default Role on Pacilian model

--- src/main/java/id/ac/ui/cs/advprog/beauthentication/model/Pacilian.java
+++ src/main/java/id/ac/ui/cs/advprog/beauthentication/model/Pacilian.java
@@ -1,7 +1,9 @@
 package id.ac.ui.cs.advprog.beauthentication.model;
 
+import id.ac.ui.cs.advprog.beauthentication.enums.Role;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -19,4 +21,13 @@ import lombok.experimental.SuperBuilder;
 public class Pacilian extends User {
     @Column(columnDefinition = "TEXT")
     private String medicalHistory;
+    
+    @PrePersist
+    @Override
+    protected void onCreate() {
+        super.onCreate();
+        if (getRole() == null) {
+            setRole(Role.PACILIAN);
+        }
+    }
 }
