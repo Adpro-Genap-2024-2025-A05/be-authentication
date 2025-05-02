adbe171310793d7c6da71e966cdc1ecf7048196b
StevenSetiawan <stevensetiawan1802@gmail.com>
2025-05-02 11:30:06 +0700

    [GREEN] Implement Login DTO

---
+++ src/main/java/id/ac/ui/cs/advprog/beauthentication/dto/LoginDto.java
@@ -0,0 +1,22 @@
+package id.ac.ui.cs.advprog.beauthentication.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginDto {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
