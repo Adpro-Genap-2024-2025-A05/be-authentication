347793f1cd9b952ebcd5078f033d96f9e9097854
StevenSetiawan <stevensetiawan1802@gmail.com>
2025-05-02 11:28:26 +0700

    [GREEN] Implement Register Response DTO

---
+++ src/main/java/id/ac/ui/cs/advprog/beauthentication/dto/RegisterResponseDto.java
@@ -0,0 +1,17 @@
+package id.ac.ui.cs.advprog.beauthentication.dto;
+
+import id.ac.ui.cs.advprog.beauthentication.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterResponseDto {
+    private String id;
+    private Role role;
+    private String message;
+}
